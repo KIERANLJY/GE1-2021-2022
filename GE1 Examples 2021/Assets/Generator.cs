c7786d96760b140c7cf47aa41a65ec07a41fb289
Your Name <you@example.com>
2021-09-26 23:38:23 +0100

    lab1

--- GE1 Examples 2021/Assets/Generator.cs
+++ GE1 Examples 2021/Assets/Generator.cs
@@ -4,13 +4,39 @@ using UnityEngine;
 
 public class Generator : MonoBehaviour
 {
+    public GameObject prefab;
+    public float radius = 1;
+    public int points = 6;
+    public int cirNum = 10;
+
+    // Start is called before the first frame update
     void Start()
     {
+        for (int j = 0 ; j < cirNum ; j ++)
+        {
+            float theta  = (Mathf.PI * 2.0f) / (float) points;
+            for (int i = 0 ; i < points ; i ++)
+            {
+                float angle = theta * i;
+                Vector3 position = new Vector3(
+                    Mathf.Sin(angle) * radius,
+                    Mathf.Cos(angle) * radius,
+                    0
+                );
+                Debug.Log(angle);
+                Debug.Log(position);
+                GameObject obj = GameObject.Instantiate(prefab, position, Quaternion.identity);
+                obj.transform.parent = transform;
+            }
+            radius ++;
+            points += 6;
+        }
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        
     }
 }
--- GE1 Examples 2021/Assets/RotateMe.cs
+++ GE1 Examples 2021/Assets/RotateMe.cs
@@ -14,6 +14,6 @@ public class RotateMe : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-    
+        transform.Rotate(0, 30 * Time.deltaTime, 0);
     }
 }
